3b18224a72ed1b5593f85877912a86e33ac4f156
paulo-peyerl <paulo.peyerl1@gmail.com>
2025-07-21 19:11:45 -0300

    Criando o botão sortear

---
+++ app.js
@@ -0,0 +1,5 @@
+function sortear() {
+    let quantidadeDeNumeros = parseInt(document.getElementById('quantidade').value);
+    let doNumero = parseInt(document.getElementById('de').value);
+    let ateNumero = parseInt(document.getElementById('ate').value);
+}

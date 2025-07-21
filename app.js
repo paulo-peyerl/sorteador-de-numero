52a1f38f51e0523000d0115024cdd4c539b8e084
paulo-peyerl <paulo.peyerl1@gmail.com>
2025-07-21 19:52:52 -0300

    Criando a lógica do botão sortear

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ function sortear() {
     let quantidadeDeNumeros = parseInt(document.getElementById('quantidade').value);
     let doNumero = parseInt(document.getElementById('de').value);
     let ateNumero = parseInt(document.getElementById('ate').value);
+
+    let numerosSorteados = [];
+    let numerosCriados;
+
+    for(let i = 0; i <= quantidadeDeNumeros; i++){
+        numerosCriados = obterNumeroAleatorio(doNumero, ateNumero);
+        numerosSorteados.push(numerosCriados);
+    }
+
+    console.log(numerosSorteados);
 }
 
 function obterNumeroAleatorio(min, max) {
-    Math.floor(Math.random() (max - min) + 1);
+    Math.floor(Math.random() * (max - min + 1) + min);
 }
